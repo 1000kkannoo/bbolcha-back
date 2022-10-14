0e3c5173421eb688677f94c2c661152b5f6482bb
Hyun Woo <hwsa10041@gmail.com>
2022-10-15 01:16:52 +0900

    회원가입시 Response 변경으로 인한 Dto 재구성

--- src/main/java/project/BBolCha/domain/user/Dto/UserDto.java
+++ src/main/java/project/BBolCha/domain/user/Dto/UserDto.java
@@ -1,13 +1,38 @@
 package project.BBolCha.domain.user.Dto;
 
+import lombok.Builder;
 import lombok.Data;
+import project.BBolCha.domain.user.Entity.User;
+import project.BBolCha.domain.user.Entity.UserToken;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
-@Data
 public class UserDto implements Serializable {
-    private final Long id;
-    private final String name;
-    private final String email;
-    private final String pw;
+
+    @Data
+    @Builder
+    public static class register{
+        private final Long id;
+        private final String name;
+        private final String email;
+        private final String pw;
+    }
+    @Data
+    @Builder
+    public static class registerResponse{
+        private final String name;
+        private final String email;
+        private final String atk;
+        private final String rtk;
+
+        public static registerResponse response(User user, String atk, UserToken rtk){
+            return registerResponse.builder()
+                    .email(user.getEmail())
+                    .name(user.getName())
+                    .atk(atk)
+                    .rtk(rtk.getToken())
+                    .build();
+        }
+    }
 }
