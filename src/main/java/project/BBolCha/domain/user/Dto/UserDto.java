37ad15b4d63daac6c37b919546c736b3376b4a21
Hyun Woo <hwsa10041@gmail.com>
2022-10-17 22:28:09 +0900

    토큰 만료시 재발급 Dto 구현

--- src/main/java/project/BBolCha/domain/user/Dto/UserDto.java
+++ src/main/java/project/BBolCha/domain/user/Dto/UserDto.java
@@ -3,7 +3,6 @@ package project.BBolCha.domain.user.Dto;
 import lombok.Builder;
 import lombok.Data;
 import project.BBolCha.domain.user.Entity.User;
-import project.BBolCha.domain.user.Entity.UserToken;
 
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
@@ -12,48 +11,72 @@ public class UserDto implements Serializable {
 
     @Data
     @Builder
-    public static class register{
+    public static class register {
         private final Long id;
         private final String name;
         private final String email;
         private final String pw;
     }
+
     @Data
     @Builder
-    public static class registerResponse{
+    public static class registerResponse {
         private final String name;
         private final String email;
         private final String atk;
         private final String rtk;
 
-        public static registerResponse response(User user, String atk, UserToken rtk){
+        public static registerResponse response(String name, String email, String atk, String rtk) {
             return registerResponse.builder()
-                    .email(user.getEmail())
-                    .name(user.getName())
+                    .email(email)
+                    .name(name)
                     .atk(atk)
-                    .rtk(rtk.getToken())
+                    .rtk(rtk)
                     .build();
         }
     }
 
     @Data
     @Builder
-    public static class login{
+    public static class login {
         private final String pw;
         private final String email;
     }
 
     @Data
     @Builder
-    public static class loginResponse{
+    public static class reissue {
+        private final String pw;
+        private final String email;
+        private final String rtk;
+    }
+
+    @Data
+    @Builder
+    public static class loginResponse {
         private final String atk;
         private final String rtk;
-        public static loginResponse response(String atk, UserToken rtk){
+
+        public static loginResponse response(String atk, String rtk) {
             return loginResponse.builder()
                     .atk(atk)
-                    .rtk(rtk.getToken())
+                    .rtk(rtk)
                     .build();
         }
     }
 
+    @Data
+    @Builder
+    public static class infoResponse {
+
+        private final String name;
+        private final String email;
+
+        public static UserDto.infoResponse response(@NotNull String name, @NotNull String email) {
+            return UserDto.infoResponse.builder()
+                    .email(email)
+                    .name(name)
+                    .build();
+        }
+    }
 }
