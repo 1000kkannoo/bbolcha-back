ed82620ab824299669f923129715d54710ec9cd8
Hyun Woo <hwsa10041@gmail.com>
2022-10-17 22:29:48 +0900

    토큰 재발급 및 정보 조회 기능 추가 _ Controller

--- src/main/java/project/BBolCha/domain/user/Controller/UserController.java
+++ src/main/java/project/BBolCha/domain/user/Controller/UserController.java
@@ -1,12 +1,13 @@
 package project.BBolCha.domain.user.Controller;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import project.BBolCha.domain.user.Dto.UserDto;
 import project.BBolCha.domain.user.Service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping("auth")
@@ -23,12 +24,27 @@ public class UserController {
         return userService.register(request);
     }
 
-    //로그인
+    // 로그인
     @PostMapping("login")
-    public ResponseEntity<UserDto.loginResponse> register(
+    public ResponseEntity<UserDto.loginResponse> login(
             @RequestBody UserDto.login request
-    ){
+    ) {
         return userService.login(request);
     }
 
+    // 로그인 만료시 atk 재발급
+    @GetMapping("reissue")
+    public ResponseEntity<UserDto.loginResponse> reissue(
+            HttpServletRequest headerRequest
+    ) {
+        return userService.reissue(headerRequest);
+    }
+
+    // 정보 조회
+    @GetMapping
+    public ResponseEntity<UserDto.infoResponse> read(
+            HttpServletRequest headerRequest
+    ){
+        return userService.read(headerRequest);
+    }
 }
