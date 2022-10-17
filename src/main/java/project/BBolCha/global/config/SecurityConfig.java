a1a4f9427e9e9513bdf3d7e4df378502e883ed5e
Hyun Woo <hwsa10041@gmail.com>
2022-10-17 22:25:54 +0900

    재발급과정 url 허용

--- src/main/java/project/BBolCha/global/config/SecurityConfig.java
+++ src/main/java/project/BBolCha/global/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/auth","/auth/login").permitAll()
+                .antMatchers("/auth","/auth/login","/auth/reissue").permitAll()
                 .anyRequest().authenticated()
 
                 .and()
