d8f5fff3f59a789595e936e240233cff040bb18c
Hyun Woo <hwsa10041@gmail.com>
2022-10-20 17:14:32 +0900

    Redis를 이용한 블랙리스트 검증을 위한 Filter 추가

--- src/main/java/project/BBolCha/global/config/Jwt/JwtFilter.java
+++ src/main/java/project/BBolCha/global/config/Jwt/JwtFilter.java
@@ -1,12 +1,18 @@
 package project.BBolCha.global.config.Jwt;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
+import project.BBolCha.global.Exception.CustomErrorCode;
+import project.BBolCha.global.Exception.CustomException;
+import project.BBolCha.global.config.RedisDao;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -16,44 +22,47 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Slf4j
+@RequiredArgsConstructor
 public class JwtFilter extends GenericFilterBean {
+    private final RedisDao redisDao;
 
-   private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
+    private final TokenProvider tokenProvider;
 
-   private final TokenProvider tokenProvider;
+    public JwtFilter(TokenProvider tokenProvider, RedisDao redisDao) {
+        this.tokenProvider = tokenProvider;
+        this.redisDao = redisDao;
+    }
 
-   public JwtFilter(TokenProvider tokenProvider) {
-      this.tokenProvider = tokenProvider;
-   }
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String jwt = resolveToken(httpServletRequest);
 
-   @Override
-   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-      throws IOException, ServletException {
-      HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-      String jwt = resolveToken(httpServletRequest);
+        log.info("----------------------------------");
+        log.info(jwt);
+        log.info("----------------------------------");
 
-      log.info("----------------------------------");
-      log.info(jwt);
-      log.info("----------------------------------");
+        String requestURI = httpServletRequest.getRequestURI();
 
-      String requestURI = httpServletRequest.getRequestURI();
+        if (jwt != null && tokenProvider.validateToken(jwt)) {
+            if(redisDao.getValues(jwt) == null){
+                Authentication authentication = tokenProvider.getAuthentication(jwt);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                log.info("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+            }
+        } else {
+            log.info("유효한 JWT 토큰이 없습니다, uri {}", requestURI);
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
 
-      if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
-         Authentication authentication = tokenProvider.getAuthentication(jwt);
-         SecurityContextHolder.getContext().setAuthentication(authentication);
-         logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
-      } else {
-         logger.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
-      }
-      filterChain.doFilter(servletRequest, servletResponse);
-   }
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
 
-   private String resolveToken(HttpServletRequest request) {
-      String bearerToken = request.getHeader("Authorization");
-
-      if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-         return bearerToken.substring(7);
-      }
-      return null;
-   }
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
 }
