64a906bf972ee21820cd3692188adf686a8e9442
Hyun Woo <hwsa10041@gmail.com>
2022-10-20 17:19:39 +0900

    변경된 API 주소 및 로그아웃 API 주소 추가

--- src/main/java/project/BBolCha/domain/user/Controller/UserController.java
+++ src/main/java/project/BBolCha/domain/user/Controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.BBolCha.domain.user.Dto.UserDto;
 import project.BBolCha.domain.user.Service.UserService;
+import project.BBolCha.global.Model.Status;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -16,14 +17,6 @@ public class UserController {
 
     private final UserService userService;
 
-    // 회원가입
-    @PostMapping
-    public ResponseEntity<UserDto.registerResponse> register(
-            @RequestBody UserDto.register request
-    ) {
-        return userService.register(request);
-    }
-
     // 로그인
     @PostMapping("login")
     public ResponseEntity<UserDto.loginResponse> login(
@@ -32,19 +25,34 @@ public class UserController {
         return userService.login(request);
     }
 
-    // 로그인 만료시 atk 재발급
-    @GetMapping("reissue")
-    public ResponseEntity<UserDto.loginResponse> reissue(
-            HttpServletRequest headerRequest
+    // 회원가입
+    @PostMapping
+    public ResponseEntity<UserDto.registerResponse> register(
+            @RequestBody UserDto.register request
     ) {
-        return userService.reissue(headerRequest);
+        return userService.register(request);
+    }
+
+    // 로그인 만료시 atk 재발급
+    @GetMapping
+    public ResponseEntity<UserDto.loginResponse> reissue(
+            @RequestHeader(value = "REFRESH_TOKEN") String rtk
+    ) {
+        return userService.reissue(rtk);
     }
 
     // 정보 조회
-    @GetMapping
+    @GetMapping("info")
     public ResponseEntity<UserDto.infoResponse> read(
-            HttpServletRequest headerRequest
     ){
-        return userService.read(headerRequest);
+        return userService.read();
+    }
+
+    // 로그아웃
+    @DeleteMapping("logout")
+    public ResponseEntity<Status> logout(
+            @RequestHeader(value = "Authorization") String auth
+    ){
+        return userService.logout(auth);
     }
 }
